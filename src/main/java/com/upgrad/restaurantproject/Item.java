17ee5667b9afd35dec0d1105867095e6ad8a2c68
dangvohiep <49779915+dangvohiep@users.noreply.github.com>
2022-07-21 01:32:37 +0700

    Add files via upload

--- src/main/java/com/upgrad/restaurantproject/Item.java
+++ src/main/java/com/upgrad/restaurantproject/Item.java
@@ -1,1 +1,25 @@
-.
+package com.upgrad.restaurantproject;
+
+public class Item {
+    private String name;
+    private int price;
+
+    public Item(String name, int price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPrice() { return price;}
+
+    @Override
+    public String toString(){
+        return  name + ":"
+                + price
+                + "\n"
+                ;
+    }
+}
